bb52c6ec65af41e0ee7530b36cc624915f8d2e9b
Grisha <sibrim1212@gmail.com>
2024-11-09 20:37:46 +0400

    add click on password to copy in buffer

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ function generatePassword() {
     
     document.getElementById('password').value = password;
 }
+
+document.getElementById("password").addEventListener("click", function(){
+    const passwordField = document.getElementById("password");
+    passwordField.select();
+    passwordField.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(passwordField.value)
+        .then(() => {
+            alert('Password copied to clipboard!');
+        })
+        .catch(err => {
+            console.error('Failed to copy password: ', err);
+            alert('Failed to copy password.');
+        });
+})
