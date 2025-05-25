1cb53cf9f026145b694799264afac9f133691639
gusevgrishaem1 <sibrim1212@gmail.com>
2025-05-25 19:33:10 +0400

    refactor code

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+let passwordLength = 15;
+
+function updateLengthDisplay() {
+    passwordLength = parseInt(document.getElementById('lengthRange').value, 10);
+    document.getElementById('lengthValue').textContent = passwordLength;
+}
+
 function generatePassword() {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+';
-    const passwordLength = 12; // Length of the password to generate
     let password = '';
-    
+
     for (let i = 0; i < passwordLength; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        password += characters[randomIndex];
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters[randomIndex];
     }
-    
+
     document.getElementById('password').value = password;
 }
 
@@ -20,29 +26,33 @@ function showMsg(msg) {
     container.appendChild(messageDiv);
 
     setTimeout(() => {
-        messageDiv.style.opacity = '1';
-        messageDiv.style.visibility = 'visible';
+    messageDiv.style.opacity = '1';
+    messageDiv.style.visibility = 'visible';
     }, 10);
 
     setTimeout(() => {
-        messageDiv.style.opacity = '0';
-        messageDiv.style.visibility = 'hidden';
-        setTimeout(() => {
-            container.removeChild(messageDiv);
-        }, 500);
+    messageDiv.style.opacity = '0';
+    messageDiv.style.visibility = 'hidden';
+    setTimeout(() => {
+        container.removeChild(messageDiv);
+    }, 500);
     }, 3000);
 }
 
-document.getElementById("password").addEventListener("click", function(){
+document.getElementById("password").addEventListener("click", function () {
     const passwordField = document.getElementById("password");
     passwordField.select();
     passwordField.setSelectionRange(0, 99999);
     navigator.clipboard.writeText(passwordField.value)
-        .then(() => {
-            showMsg('Password copied to clipboard!')
-        })
-        .catch(err => {
-            console.error('Failed to copy password: ', err);
-            showMsg('Failed to copy password.');
-        });
-})
+    .then(() => {
+        showMsg('Password copied to clipboard!');
+    })
+    .catch(err => {
+        console.error('Failed to copy password: ', err);
+        showMsg('Failed to copy password.');
+    });
+});
+
+// инициализация при загрузке
+updateLengthDisplay();
+generatePassword();
